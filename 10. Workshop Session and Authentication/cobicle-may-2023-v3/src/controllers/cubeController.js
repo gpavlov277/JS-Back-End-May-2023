961c5b1af014127e18c29192d6a9e04bef739f84
gpavlov7 <gpavlov277@gmail.com>
2023-06-13 13:20:49 +0300

    Add route guards

--- 10. Workshop Session and Authentication/cobicle-may-2023-v3/src/controllers/cubeController.js
+++ 10. Workshop Session and Authentication/cobicle-may-2023-v3/src/controllers/cubeController.js
@@ -1,22 +1,23 @@
 const router = require("express").Router();
 
 const cubeManager = require("../managers/cubeManager.js");
+const { isAuth } = require("../middlewares/authMiddleware.js");
 const accessoryManager = require("../managers/accessoryManager.js");
 
 const { difficultyOptionViewData } = require("../utils/viewHelpers.js");
 
-router.get("/create", (req, res) => {
+router.get("/create", isAuth, (req, res) => {
   console.log(req.user);
   res.render("cube/create");
 });
-router.post("/create", async (req, res) => {
+router.post("/create", isAuth, async (req, res) => {
   const { name, description, imageUrl, difficultyLevel } = req.body;
   await cubeManager.create({
     name,
     description,
     imageUrl,
     difficultyLevel: Number(difficultyLevel),
-    owner: req.user._id,
+    owner: req.user?._id,
   });
   res.redirect("/");
 });
@@ -29,24 +30,25 @@ router.get("/:cubeId/details", async (req, res) => {
   if (!cube) {
     return res.redirect("/404");
   }
-  res.render("cube/details", cube);
+  const isOwner = cube.owner == req?.user?._id;
+  res.render("cube/details", { cube, isOwner });
 });
 
-router.get("/:cubeId/attach-accessory", async (req, res) => {
+router.get("/:cubeId/attach-accessory", isAuth, async (req, res) => {
   const cube = await cubeManager.getOne(req.params.cubeId).lean();
   const accessories = await accessoryManager.getOthers(cube.accessories).lean();
   const hasAccessories = accessories.length > 0;
   res.render("accessory/attach", { cube, accessories, hasAccessories });
 });
 
-router.post("/:cubeId/attach-accessory", async (req, res) => {
+router.post("/:cubeId/attach-accessory", isAuth, async (req, res) => {
   const { accessory: accessoryId } = req.body;
   const cubeId = req.params.cubeId;
   await cubeManager.attachAccessory(cubeId, accessoryId);
   res.redirect(`/cubes/${cubeId}/details`);
 });
 
-router.get("/:cubeId/delete", async (req, res) => {
+router.get("/:cubeId/delete", isAuth, async (req, res) => {
   const cube = await cubeManager.getOne(req.params.cubeId).lean();
 
   const options = difficultyOptionViewData(cube.difficultyLevel);
--- 10. Workshop Session and Authentication/cobicle-may-2023-v3/src/middlewares/authMiddleware.js
+++ 10. Workshop Session and Authentication/cobicle-may-2023-v3/src/middlewares/authMiddleware.js
@@ -8,14 +8,22 @@ exports.auth = async (req, res, next) => {
       const user = await jwt.verify(token, SECRET);
 
       req.user = user;
+      res.locals.user = user;
+      res.locals.isAuthenticated = true;
 
       return next();
     } catch (error) {
       res.clearCookie("auth");
-
-      return res.rediret("/users/login");
+      res.redirect("/users/login");
     }
   } else {
     next();
   }
+};
+
+exports.isAuth = (req, res, next) => {
+  if (!req.user) {
+    return res.redirect("/users/login");
+  }
+  next();
 };
